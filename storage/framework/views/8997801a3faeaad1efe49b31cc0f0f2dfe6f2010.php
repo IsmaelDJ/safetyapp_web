a77d77776cc7a2045ba3f78b5c364db868197616
ismaeldjekale <ismaeldjekale@gmail.com>
2022-06-22 06:08:53 +0100

    Adding Forge

---
+++ storage/framework/views/8997801a3faeaad1efe49b31cc0f0f2dfe6f2010.php
@@ -0,0 +1,245 @@
+<?php $__env->startSection('title'); ?> Reponses <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+    <!-- Bootstrap Css -->
+    <link href="<?php echo e(URL::asset('/assets/css/bootstrap.min.css')); ?>" id="bootstrap-style" rel="stylesheet"
+          type="text/css"/>
+    <!-- Icons Css -->
+    <link href="<?php echo e(URL::asset('/assets/css/icons.min.css')); ?>" rel="stylesheet" type="text/css"/>
+    <!-- App Css-->
+    <link href="<?php echo e(URL::asset('/assets/css/app.min.css')); ?>" id="app-style" rel="stylesheet" type="text/css"/>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+    <div class="card">
+
+        <div class="row g-0">
+            <form action="<?php echo e(route('quiz_responses.store')); ?>" method="post" enctype="multipart/form-data">
+                <?php echo csrf_field(); ?>
+
+                <div class="row">
+
+
+                    <div class="col-md-5 col-xl-4">
+                        <div class="auth-full-page-content p-md-5 p-4">
+                            <div class="w-100">
+
+                                <div class="mb-3">
+                                    <div>
+                                        <label for="image" class="form-label">Image</label>
+                                    </div>
+
+                                    <img id="ruleImage" class="img-fluid mb-1"
+                                         src="<?php echo e(URL::asset('assets/images/placeholder_rule.png')); ?>">
+
+                                    <input class=" form-control form-control-lg <?php $__errorArgs = ['image'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                           id="image"
+                                           name="image" type="file" onchange="PreviewImage();">
+                                    <?php $__errorArgs = ['image'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                    <strong>Image invalide</strong>
+                                </span>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-7 col-xl-7">
+                        <div class="bg-soft pt-lg-5 p-4 col-md-10">
+                            <div class="w-100">
+
+                                <label class="col-form-label">Réponse</label>
+                                <div>
+                                    <select
+                                        class="form-select form-select-lg <?php $__errorArgs = ['correct'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                        name="correct">
+                                        <option selected>Selectionnez la valeur de la reponse</option>
+                                        <option value="true">
+                                            Reponse correcte
+                                        </option>
+                                        <option value="false">
+                                            Reponse incorrecte
+                                        </option>
+                                    </select>
+                                    <?php $__errorArgs = ['correct'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                    <strong>Valeur de reponse non selectionnée</strong>
+                                </span>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+
+                                <div class="mb-3 mt-3">
+                                    <label for="description" class="form-label">Description</label>
+                                    <textarea id="" description placeholder="Entrez la question"
+                                              name="description"
+                                              class="form-control form-control-lg <?php $__errorArgs = ['description'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"><?php echo e(old('description')); ?></textarea>
+                                    <?php $__errorArgs = ['description'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                    <span class="invalid-feedback" role="alert">
+                                    <strong>Question invalid</strong>
+                                </span>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+
+
+                                <div class="mt-5">
+                                    <div class="mb-3">
+                                        <label for="fr" class="form-label">Audio Français</label>
+                                        <input class="form-control form-control-lg <?php $__errorArgs = ['fr'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                               id="fr"
+                                               value="<?php echo e(old('fr')); ?>"
+                                               name="fr" type="file">
+                                        <?php $__errorArgs = ['fr'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <span class="invalid-feedback" role="alert">
+                                    <strong>Audio invalide</strong>
+                                </span>
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </div>
+
+
+                                    <div class="mb-3">
+                                        <label for="ar" class="form-label">Audio Arabe</label>
+                                        <input class="form-control form-control-lg <?php $__errorArgs = ['ar'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                               id="ar"
+                                               value="<?php echo e(old('ar')); ?>"
+                                               name="ar" type="file">
+                                        <?php $__errorArgs = ['ar'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <span class="invalid-feedback" role="alert">
+                                    <strong>Audio invalide</strong>
+                                </span>
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="ng" class="form-label">Audio Ngambaye</label>
+                                        <input class="form-control form-control-lg <?php $__errorArgs = ['ng'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+                                               id="ng"
+                                               value="<?php echo e(old('ng')); ?>"
+                                               name="ng" type="file">
+                                        <?php $__errorArgs = ['ng'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <span class="invalid-feedback" role="alert">
+                                    <strong>Audio invalide</strong>
+                                </span>
+                                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <button type="submit" class="btn btn-primary w-md">Enregistrer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <input type="hidden" name="quiz_question_id" value="<?php echo e($quizQuestion->id); ?>">
+
+            </form>
+        </div>
+        <!-- end row -->
+    </div>
+
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+    <script>
+        function PreviewImage() {
+            var oFReader = new FileReader();
+            oFReader.readAsDataURL(document.getElementById("image").files[0]);
+
+            oFReader.onload = function (oFREvent) {
+                document.getElementById("ruleImage").src = oFREvent.target.result;
+            };
+        };
+    </script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/MAMP/htdocs/safetyapp/resources/views/quiz_responses/create.blade.php ENDPATH**/ ?>
