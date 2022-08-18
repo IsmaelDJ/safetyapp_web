eb1460246fa725628ebcf0a8edf610760ecb5a95
Ismae <ismaeldjekale@gmail.com>
2022-08-18 19:20:16 +0100

    quiz module

--- app/Http/Controllers/Api/QuizQuestionController.php
+++ app/Http/Controllers/Api/QuizQuestionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Models\QuizQuestion;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\UploadedFile ;
 
 /**
  * @OA\Get(
