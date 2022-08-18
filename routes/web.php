1c97006f2a37aff0408abfa43db1091e1d8c6b37
Ismae <ismaeldjekale@gmail.com>
2022-08-18 20:16:53 +0100

    quiz module

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,7 @@
 use App\Http\Controllers\ContractorController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EmployeeQuizResponseController;
-<<<<<<< HEAD
 use App\Http\Controllers\QuizQuestionController;
-=======
-use App\Http\Controllers\QuizController;
-use App\Http\Controllers\QuizQuestionController;
-use App\Http\Controllers\QuizResponseController;
->>>>>>> 2a47303148ee144fd7a50e625d3a19b1a897ba60
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\RuleController;
@@ -53,7 +47,6 @@ Route::resource('employees', EmployeeController::class);
 //contractor employee
 Route::get('contractor_employees/{contractor}', [ContractorController::class,'export_employees'])->name('contractor_employees');
 
-<<<<<<< HEAD
 //quiz question
 Route::resource('quiz_questions', QuizQuestionController::class);
 
@@ -66,33 +59,6 @@ Route::resource('employee_quiz_responses', EmployeeQuizResponseController::class
 Route::get('employee_quiz_responses', [EmployeeQuizResponseController::class,'index'])->name('employee_quiz_responses.index');
 Route::get('employee_quiz_responses/{quiz_question_id}/quizzes', [EmployeeQuizResponseController::class,'quizzes'])->name('employee_quiz_responses.quizzes');
 Route::get('employee_quiz_responses/{employee_id}/employees', [EmployeeQuizResponseController::class,'employees'])->name('employee_quiz_responses.employees');
-=======
-//quiz
-Route::resource('quizzes', QuizController::class);
-
-//Route::resource('quiz_questions', QuizQuestionController::class);
-
-Route::get('quiz_questions', [QuizQuestionController::class,'index'])->name('quiz_questions.index');
-Route::post('quiz_questions', [QuizQuestionController::class,'store'])->name('quiz_questions.store');
-Route::get('quiz_questions/create/{quiz}', [QuizQuestionController::class,'create'])->name('quiz_questions.create');
-Route::get('quiz_questions/{quiz_question}', [QuizQuestionController::class,'show'])->name('quiz_questions.show');
-Route::put('quiz_questions/{quiz_question}', [QuizQuestionController::class,'update'])->name('quiz_questions.update');
-Route::delete('quiz_questions/{quiz_question}', [QuizQuestionController::class,'destroy'])->name('quiz_questions.destroy');
-Route::get('quiz_questions/{quiz_question}/edit', [QuizQuestionController::class,'edit'])->name('quiz_questions.edit');
-
-Route::get('quiz_responses', [QuizResponseController::class,'index'])->name('quiz_responses.index');
-Route::post('quiz_responses', [QuizResponseController::class,'store'])->name('quiz_responses.store');
-Route::get('quiz_responses/create/{quiz_question}', [QuizResponseController::class,'create'])->name('quiz_responses.create');
-Route::get('quiz_responses/{quiz_response}', [QuizResponseController::class,'show'])->name('quiz_responses.show');
-Route::put('quiz_responses/{quiz_response}', [QuizResponseController::class,'update'])->name('quiz_responses.update');
-Route::delete('quiz_responses/{quiz_response}', [QuizResponseController::class,'destroy'])->name('quiz_responses.destroy');
-Route::get('quiz_responses/{quiz_response}/edit', [QuizResponseController::class,'edit'])->name('quiz_responses.edit');
-
-Route::get('employee_quiz_responses', [EmployeeQuizResponseController::class,'index'])->name('employee_quiz_responses.index');
-Route::get('employee_quiz_responses/{employee_id}/{quiz_id}', [EmployeeQuizResponseController::class,'employee'])->name('employee_quiz_responses.employee');
-
-
->>>>>>> 2a47303148ee144fd7a50e625d3a19b1a897ba60
 Route::get('{any}', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 
 
