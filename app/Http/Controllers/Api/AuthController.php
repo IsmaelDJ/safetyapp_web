c22110e79649a3732a449966de82d6b2bc322fa9
Ismae <ismaeldjekale@gmail.com>
2022-08-25 05:30:19 +0100

    get user details

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,18 +14,31 @@ class AuthController extends Controller
 
      public function login(Request $request){
 
-        if (!$request->only('uid', 'password')) {
+        try{
+            if (!$request->only('uid', 'password')) {
+                return response()->json([
+                    'message' => 'Information de connection invalid'
+                ], 401);
+            }
+    
+            $employee = Employee::where('uid', $request['uid'])->firstOrFail();
+    
+            if(!($employee->password == $request('password'))){
+                return response()->json([
+                    'message' => 'Mot de passe incorrect'
+                ], 401);
+            }
+    
             return response()->json([
-                'message' => 'Information de connection invalid'
-            ], 401);
+                'status'       =>  true,
+                'message'      => 'Connection reuissi',
+                'employee'     =>  $employee,
+            ]);
+        }catch(Throwable $th){
+           
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        $employee = Employee::where('uid', $request['uid'])->firstOrFail();
-
-        return response()->json([
-            'status'       =>  true,
-            'message'      => 'Connection reuissi',
-            'employee'     =>  $employee,
-        ]);
     }
 }
