d342013e7f907f392c601c9e64f1a3e8ff0a5705
Gnandal Elisée <elisee@ismatech.co>
2023-06-30 17:42:07 +0100

    Correction de la maquette

--- resources/views/rules/show.blade.php
+++ resources/views/rules/show.blade.php
@@ -73,7 +73,7 @@
                         @foreach($sameCategoryRules as $rule)
                         <div class="rule-item col-4">
                             <div class="card mb-2 rounded" style="background-image:linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(255, 255, 255, 0)), url({{ URL::asset($rule->image) }}); background-size: 100%; background-repeat: no-repeat; background-position: center;">
-                                <div class="d-flex justify-content-end flex-column" style="height: 290px">
+                                <div class="d-flex justify-content-end flex-column" style="height: 300px">
                                     <a href="{{ route("rules.show", $rule->id) }}" style="height: 100%; width: 100%;"></a>
                                     <div class="d-flex justify-content-between">
                                         <div class="d-xl-flex justify-content-between align-self-center">
