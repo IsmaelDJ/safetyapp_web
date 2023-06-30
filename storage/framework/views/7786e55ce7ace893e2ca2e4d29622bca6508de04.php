63cc820f1787354032875b7532b28eb77ef12809
Gnandal Elisée <elisee@ismatech.co>
2023-06-30 17:41:14 +0100

    Correction de la maquette

--- storage/framework/views/7786e55ce7ace893e2ca2e4d29622bca6508de04.php
+++ storage/framework/views/7786e55ce7ace893e2ca2e4d29622bca6508de04.php
@@ -71,7 +71,7 @@
                         <?php $__currentLoopData = $sameCategoryRules; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rule): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                         <div class="rule-item col-4">
                             <div class="card mb-2 rounded" style="background-image:linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(255, 255, 255, 0)), url(<?php echo e(URL::asset($rule->image)); ?>); background-size: 100%; background-repeat: no-repeat; background-position: center;">
-                                <div class="d-flex justify-content-end flex-column" style="height: 280px">
+                                <div class="d-flex justify-content-end flex-column" style="height: 290px">
                                     <a href="<?php echo e(route("rules.show", $rule->id)); ?>" style="height: 100%; width: 100%;"></a>
                                     <div class="d-flex justify-content-between">
                                         <div class="d-xl-flex justify-content-between align-self-center">
