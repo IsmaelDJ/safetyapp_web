eadcbfab4bd77b5dfe22d56635e0b54b42026630
Yamingue <yamking01@gmail.com>
2024-04-27 16:10:10 +0100

    add drivers.user_id in groupe by

--- app/Http/Controllers/DriverQuizResponseController.php
+++ app/Http/Controllers/DriverQuizResponseController.php
@@ -42,7 +42,7 @@ class DriverQuizResponseController extends Controller
             ->selectRaw('COUNT(CASE WHEN driver_quiz_responses.correct = 0 THEN 1 END) as incorrect_answers')
             ->leftJoin('driver_quiz_responses', 'drivers.id', '=', 'driver_quiz_responses.driver_id')
             ->where('drivers.role', '=', 'driver')
-            ->groupBy('drivers.id')
+            ->groupBy('drivers.id', 'drivers.user_id')
             ->orderByDesc('correct_answers') // Tri par le nombre de bonnes réponses, du plus grand au plus petit
             // ->orderBy('incorrect_answers') // Tri par le nombre de mauvaises réponses, du plus petit au plus grand
             ->get();
